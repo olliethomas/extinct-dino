4dd958dffbaeae26a3f8706f515933d52f4210fa
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2021-06-04 11:23:23 +0100

    add test that runs all (both) models for 1 step

---
+++ tests/model_test.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from typing import Final, List
+
+from hydra import compose, initialize
+from hydra.utils import instantiate
+from omegaconf import OmegaConf
+import pytest
+
+from extinct.main import Config, start
+
+CFG_PTH: Final[str] = "../extinct/configs"
+SCHEMAS: Final[List[str]] = [
+    "data=celeba_local",
+    "exp=unit_test",
+    "trainer=unit_test",
+]
+
+
+@pytest.mark.parametrize("model_schema", ["erm", "kc"])
+def test_entrypoint(model_schema: str) -> None:
+    """Quick run on models to check nothing's broken.
+
+    Use this if you need an entrypoint.
+    """
+    with initialize(config_path=CFG_PTH):
+        # config is relative to a module
+        hydra_cfg = compose(
+            config_name="main",
+            overrides=[f"model={model_schema}"] + SCHEMAS,
+        )
+        if hasattr(hydra_cfg.data, "data_dir"):
+            hydra_cfg.data.data_dir = Path(hydra_cfg.data.data_dir).expanduser()
+        cfg: Config = instantiate(hydra_cfg, _recursive_=True, _convert_="partial")
+        start(cfg, raw_config=OmegaConf.to_container(hydra_cfg, resolve=True, enum_to_str=True))
