b880bd6952bc704acd3e9de2e33e0a121c8e9d89
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-06-14 18:40:25 +0100

    Modify extract_labels to work in the most general case.

--- extinct/datamodules/utils.py
+++ extinct/datamodules/utils.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Union, cast
+from typing import Union, cast, Iterable
 from typing_extensions import TypeAlias
 
 import ethicml.vision as emvi
@@ -15,7 +15,9 @@ _Dataset: TypeAlias = Union[emvi.TorchImageDataset, TiWrapper]
 ExtractableDataset: TypeAlias = Union[ConcatDataset[_Dataset], _Dataset]
 
 
-def extract_labels_from_dataset(dataset: ExtractableDataset) -> tuple[Tensor, Tensor]:
+def extract_labels_from_dataset(
+    dataset: Iterable[tuple[Tensor, Tensor, Tensor]]
+) -> tuple[Tensor, Tensor]:
     def _extract(dataset: _Dataset) -> tuple[Tensor, Tensor]:
         if isinstance(dataset, Subset):
             _s = cast(Tensor, dataset.dataset.s[dataset.indices])  # type: ignore
@@ -25,14 +27,22 @@ def extract_labels_from_dataset(dataset: ExtractableDataset) -> tuple[Tensor, Te
             _y = dataset.y
         return _s, _y
 
-    if isinstance(dataset, ConcatDataset):
+    try:
+        if isinstance(dataset, ConcatDataset):
+            s_all_ls, y_all_ls = [], []
+            for _dataset in dataset.datasets:
+                s, y = _extract(_dataset)  # type: ignore
+                s_all_ls.append(s)
+                y_all_ls.append(y)
+            s_all = torch.cat(s_all_ls, dim=0)
+            y_all = torch.cat(y_all_ls, dim=0)
+        else:
+            s_all, y_all = _extract(dataset)  # type: ignore
+    except AttributeError:
         s_all_ls, y_all_ls = [], []
-        for _dataset in dataset.datasets:
-            s, y = _extract(_dataset)  # type: ignore
+        for _, s, y in dataset:
             s_all_ls.append(s)
             y_all_ls.append(y)
         s_all = torch.cat(s_all_ls, dim=0)
         y_all = torch.cat(y_all_ls, dim=0)
-    else:
-        s_all, y_all = _extract(dataset)  # type: ignore
     return s_all, y_all
