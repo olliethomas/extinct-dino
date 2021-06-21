9927c3875a56b84ef0c6e8582c584b3e5c14a5be
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-06-21 23:15:03 +0100

    Don't nest compose.

--- extinct/datamodules/dino.py
+++ extinct/datamodules/dino.py
@@ -14,30 +14,26 @@ class DinoAugmentation(A.ImageOnlyTransform):
         local_crops_scale: tuple[float, float],
         local_crops_number: int,
     ) -> None:
-        flip_and_color_jitter = A.Compose(
-            [
-                A.HorizontalFlip(p=0.5),
-                A.ColorJitter(p=0.8, brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1),
-                A.ToGray(p=0.2),
-            ]
-        )
-        normalize = A.Compose(
-            [
-                ToTensorV2(),
-                # The default per-mean/std values for albumentations.Normalize
-                # match the ImageNet ones prescribed by DINO
-                A.Normalize(),
-                A.ToFloat(),
-            ]
-        )
+        flip_and_color_jitter = [
+            A.HorizontalFlip(p=0.5),
+            A.ColorJitter(p=0.8, brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1),
+            A.ToGray(p=0.2),
+        ]
+        normalize = [
+            ToTensorV2(),
+            # The default per-mean/std values for albumentations.Normalize
+            # match the ImageNet ones prescribed by DINO
+            A.Normalize(),
+            A.ToFloat(),
+        ]
         self.global_transfo1 = A.Compose(
             [
                 A.RandomResizedCrop(
                     height=224, width=224, scale=global_crops_scale, interpolation=cv2.INTER_CUBIC  # type: ignore
                 ),
-                flip_and_color_jitter,
+                *flip_and_color_jitter,
                 A.GaussianBlur(p=1.0, sigma_limit=(0.1, 2)),
-                normalize,
+                *normalize,
             ]
         )
         self.global_transfo2 = A.Compose(
@@ -45,10 +41,10 @@ class DinoAugmentation(A.ImageOnlyTransform):
                 A.RandomResizedCrop(
                     height=224, width=224, scale=global_crops_scale, interpolation=cv2.INTER_CUBIC  # type: ignore
                 ),
-                flip_and_color_jitter,
+                *flip_and_color_jitter,
                 A.GaussianBlur(p=1.0, sigma_limit=(0.1, 2)),
                 A.Solarize(p=0.2),
-                normalize,
+                *normalize,
             ]
         )
         # transformation for the local small crops
@@ -58,9 +54,9 @@ class DinoAugmentation(A.ImageOnlyTransform):
                 A.RandomResizedCrop(
                     height=96, width=96, scale=local_crops_scale, interpolation=cv2.INTER_CUBIC  # type: ignore
                 ),
-                flip_and_color_jitter,
+                *flip_and_color_jitter,
                 A.GaussianBlur(p=1.0, sigma_limit=(0.1, 2)),
-                normalize,
+                *normalize,
             ]
         )
 
