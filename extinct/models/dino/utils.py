6728e507f57f309d4dad0d7990fc71d5a705b2ca
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2021-06-21 15:37:29 +0100

    remove dulpicated code

--- extinct/models/dino/utils.py
+++ extinct/models/dino/utils.py
@@ -7,47 +7,7 @@ import torch
 from torch import Tensor
 import torch.nn as nn
 
-from . import DINOHead
-from .vit import VisionTransformer
-
 __all__ = ["trunc_normal_", "cosine_scheduler", "get_params_groups"]
-
-
-class MultiCropWrapper(nn.Module):
-    """
-    Perform forward pass separately on each resolution input.
-    The inputs corresponding to a single resolution are clubbed and single
-    forward is run on the same resolution inputs. Hence we do several
-    forward passes = number of different resolutions used. We then
-    concatenate all the output features and run the head forward on these
-    concatenated features.
-    """
-
-    def __init__(self, backbone: VisionTransformer, head: DINOHead) -> None:
-        super(MultiCropWrapper, self).__init__()
-        # disable layers dedicated to ImageNet labels classification
-        backbone.fc, backbone.head = nn.Identity(), nn.Identity()
-        self.backbone = backbone
-        self.head = head
-
-    def forward(self, x: list[Tensor] | Tensor) -> Tensor:
-        # convert to list
-        if not isinstance(x, list):
-            x = [x]
-        idx_crops = torch.cumsum(
-            torch.unique_consecutive(
-                torch.tensor([inp.shape[-1] for inp in x]),
-                return_counts=True,
-            )[1],
-            0,
-        )
-        start_idx = 0
-        for end_idx in idx_crops:
-            _out = self.backbone(torch.cat(x[start_idx:end_idx]))
-            output: Tensor = _out if start_idx == 0 else torch.cat((output, _out))
-            start_idx = end_idx
-        # Run the head forward on the concatenated features.
-        return self.head(output)
 
 
 def _no_grad_trunc_normal_(tensor: Tensor, mean: float, std: float, a: float, b: float) -> Tensor:
